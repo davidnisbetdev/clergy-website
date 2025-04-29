8ca1d0cc51a53eed1264756364a5f1de457b7777
David Nisbet <70908860+davidnisbetdev@users.noreply.github.com>
2025-04-29 14:54:58 +0000

    init frontend

---
+++ ghost-clergy-site/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import MainHeader from '@/components/Header.vue'
+import MainFooter from '@/components/Footer.vue'
+import RouterLink from 'Vue Router'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="app-container">
+    <header class="site-header">
+      <div class="site-title">
+        <RouterLink to="/">The Clergy</RouterLink>
+      </div>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
