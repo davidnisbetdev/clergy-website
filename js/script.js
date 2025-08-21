ac9bbfe775b92f53352aa8f345005ff54ccc9a34
David <nizz3hx@protonmail.com>
2025-08-21 19:02:02 +0100

    Add content, change lineage page to grid.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 import { staffArr } from "./staff.js";
+import { papaArr } from "./staff.js";
 
 
 function fillStaff(staffArr) {
 let staffOutput = ''
 const staffElement = document.getElementById('staff-output')
+
+if (!staffElement){
+    return;
+}
 
 staffArr.forEach(person =>{
     staffOutput += `<section class='feature staff-card'>
@@ -21,4 +26,31 @@ staffArr.forEach(person =>{
 staffElement.innerHTML = staffOutput
 }
 
+function fillPapa(papaArr) {
+    let refactoredArr = papaArr.slice().reverse()
+    let papaOutput = ''
+    const papaElement = document.getElementById('papa-output')
+
+    if (!papaElement) {
+      return;
+    }
+
+    refactoredArr.forEach(person => {
+        papaOutput += `<section class='feature staff-card'>
+                    <div class="staff-image">
+                        <img src='${person.img}'/>
+                        <span class="staff-name">${person.papaName}</span>
+                        <aside class="staff-aka">${person.alsoKnown}</aside>
+                    </div>
+                    <section class="staff-info">
+                    <p>${person.quickBio}</p>
+                    <ul class='responsibilities-list'><ul>
+                    </section>
+                    </section>`;
+    })
+
+    papaElement.innerHTML = papaOutput
+}
+
 fillStaff(staffArr)
+fillPapa(papaArr)
--- js/staff.js
+++ js/staff.js
@@ -28,3 +28,42 @@ export const staffArr = [
     keyResponsibilities: [],
   },
 ];
+
+export const papaArr = [
+  {
+    img: "./images/papa/nihil.webp",
+    papaName: "Papa Nihil",
+    alsoKnown: "",
+    quickBio: "The one that started it all",
+  },
+  {
+    img: "./images/papa/primo.webp",
+    papaName: "Papa Emeritus I",
+    alsoKnown: "Primo",
+    quickBio: "The Second, Nihil's brother or son",
+  },
+  {
+    img: "./images/papa/secondo.webp",
+    papaName: "Papa Emeritus II",
+    alsoKnown: "Secondo",
+    quickBio: "The Third, Nihil's brother or son",
+  },
+  {
+    img: "./images/papa/terzo.webp",
+    papaName: "Papa Emeritus III",
+    alsoKnown: "Terzo",
+    quickBio: "The Fourth Nihil's brother or son",
+  },
+  {
+    img: "./images/papa/cardi.webp",
+    papaName: "Papa Emeritus IV",
+    alsoKnown: "Cardinal Copia, Cardi",
+    quickBio: "The Fifth, Nihil's son",
+  },
+  {
+    img: "./images/papa/perpetua.webp",
+    papaName: "Papa Emeritus V",
+    alsoKnown: "Perpetua, 'Fucking V'",
+    quickBio: "The Sixth, Nihil's son, Cardi's fraternal twin",
+  },
+];
