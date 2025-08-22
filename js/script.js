26fa22787996ebaabf72980dbcc6e56b0a70c353
David <nizz3hx@protonmail.com>
2025-08-22 17:13:01 +0100

    added new page, albums and ritual albums.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-import { staffArr } from "./staff.js";
-import { papaArr } from "./staff.js";
+import { staffArr, papaArr, albums } from "./staff.js";
 
 
 function fillStaff(staffArr) {
 let staffOutput = ''
 const staffElement = document.getElementById('staff-output')
 
-if (!staffElement){
+if (!staffElement || !papaElement){
     return;
 }
 
@@ -31,7 +30,7 @@ function fillPapa(papaArr) {
     let papaOutput = ''
     const papaElement = document.getElementById('papa-output')
 
-    if (!papaElement) {
+    if (!papaElement || !staffElement) {
       return;
     }
 
@@ -46,11 +45,51 @@ function fillPapa(papaArr) {
                     <p>${person.quickBio}</p>
                     <ul class='responsibilities-list'><ul>
                     </section>
-                    </section>`;
+                    </section>`
     })
 
     papaElement.innerHTML = papaOutput
 }
 
+function fillAlbums(albums){
+    const sacredElement = document.getElementById("sacredPsalms");
+    const minorElement = document.getElementById("minorPsalms");
+    const ritualElement = document.getElementById("rituals");
+
+    const sacredPsalms = albums
+        .filter(item => item.category === 'sacred')
+        .sort((a,b) => new Date(b.releaseDate) - new Date(a.releaseDate))
+    const minorPsalms = albums
+        .filter((item) => item.category === "minor")
+        .sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate));
+    const rituals = albums
+        .filter((item) => item.category === "ritualRecording")
+        .sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate));
+
+    function renderAlbumCard(albumList, reqElement) {
+            if(reqElement){
+                let html = "";
+                albumList.forEach(album => {
+                    html += `<section class='feature staff-card'>
+                        <div class="staff-image">
+                            <img src='${album.img}'/>
+                            <span class="staff-name">${album.name}</span>
+                            <aside class="staff-aka">${album.releaseDate}</aside>
+                        </div>
+                    <section class="staff-info">
+                    <p>${album.blurb}</p>
+                    </section>
+                    </section>`;
+                })
+                reqElement.innerHTML = html;
+            }
+        }
+        renderAlbumCard(sacredPsalms, sacredElement);
+        renderAlbumCard(minorPsalms, minorElement);
+        renderAlbumCard(rituals, ritualElement);
+
+}
+
 fillStaff(staffArr)
 fillPapa(papaArr)
+fillAlbums(albums)
